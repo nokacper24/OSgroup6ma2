f581fe4d4d8be5f54bc86663446daa11c61ccdda
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-02-21 13:20:57 +0100

    feat: :sparkles: working 2 clients

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
+use std::sync::{Mutex, Arc};
+
+use ticket_server::TicketServer;
 mod ticket_server;
 
 fn main() {
-    println!("Hello, world!");
+    let ticket_server = Arc::new(Mutex::new(TicketServer::new("The Matrix", 20)));
+    
+    let ticket_server1 = Arc::clone(&ticket_server);
+        std::thread::spawn(move || {
+            match ticket_server1.lock(){
+                Ok(mut ticket_server) => {
+                    if let Err(e) = ticket_server.book_tickets("Alice", 5) {
+                        println!("Error: {}", e);
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+        }});
+
+        let ticket_server2 = Arc::clone(&ticket_server);
+        std::thread::spawn(move || {
+            match ticket_server2.lock(){
+                Ok(mut ticket_server) => {
+                    if let Err(e) = ticket_server.book_tickets("Alice", 5) {
+                        println!("Error: {}", e);
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+        }});
+
+
+
+
+
 }
